7d3fa635f6cc99ad849497fbce9847a4bfb12b6b
Monishka Jha <jhamonishka@gmail.com>
2023-10-20 10:54:59 +0530

    Started

--- Fund-Chain Contracts/Migrations/programs/fund-contract/src/instructions/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod create_user;
-pub mod init_chort;
-pub mod join_chort;
-pub mod project_init;
-
-pub use create_user::*;
-pub use init_chort::*;
-pub use join_chort::*;
-pub use project_init::*;
